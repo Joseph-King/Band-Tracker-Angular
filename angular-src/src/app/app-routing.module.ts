c1d214037e05804c8337b22ae889cc09845eb3f4
Joseph King <josephck1997@gmail.com>
2020-11-27 10:58:12 -0500

    Version 0.1
    Routing enabled on site

---
+++ angular-src/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { ShowsComponent } from './components/shows/shows.component';
+import { ArtistsComponent } from './components/artists/artists.component';
+import { VenuesComponent } from './components/venues/venues.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'shows', component: ShowsComponent},
+  {path: 'artists', component: ArtistsComponent},
+  {path: 'venues', component: VenuesComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'logout', component: LogoutComponent},
+  {path: 'profile', component: ProfileComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ShowsComponent } from './components/shows/shows.component';
+import { ArtistsComponent } from './components/artists/artists.component';
+import { VenuesComponent } from './components/venues/venues.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ShowsComponent,
+    ArtistsComponent,
+    VenuesComponent,
+    LoginComponent,
+    RegisterComponent,
+    LogoutComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
